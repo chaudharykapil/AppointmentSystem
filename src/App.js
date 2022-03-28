9da3afbbfd28422ac518fcca5ef2c71323ea1ebb
chaudharykapil <chaudharykapil8279@gmail.com>
2022-03-28 23:51:14 +0530

    add sigup route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./screens/Homepage";
+import SignUpScreen from "./screens/SignUpScreen";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
+        <Route path = "/signup" element = {<SignUpScreen />} />
       </Routes>
     </BrowserRouter>
   );
