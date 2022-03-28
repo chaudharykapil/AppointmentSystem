52c576a9d8972b03aed0a943b7716f91a5e41a9f
chaudharykapil <chaudharykapil8279@gmail.com>
2022-03-28 23:51:44 +0530

    add header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,9 @@ export default function Header() {
     <div className='flex flex-row w-full px-5 py-3 font-bold bg-slate-800 text-white'>
         <div className='flex-initial w-3/6 hover:underline'>Logo</div>
         <div className='flex-1 hover:underline'>Home</div>
-        <div className='flex-1 hover:underline'>about us</div>
-        <div className='flex-1 hover:underline'>view list</div>
-        <div className='flex-1 hover:underline'>sign up</div>
+        <div className='flex-1 hover:underline'>About us</div>
+        <div className='flex-1 hover:underline'>View list</div>
+        <div className='flex-1 hover:underline'>Sign up</div>
     </div>
   )
 }
