6acbf803a1376e51ce36c903699fb84ca7a47281
chaudharykapil <chaudharykapil8279@gmail.com>
2022-03-29 12:41:00 +0530

    add signup form

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,9 +1,50 @@
+import { Paper, Button } from '@mui/material'
 import React, { Component } from 'react'
-
+import Header from '../components/Header'
+import TextField from '@mui/material/TextField';
+import {AccountCircle,Email,Lock} from '@mui/icons-material';
 export default class SignUpScreen extends Component {
   render() {
     return (
-      <div>SignUpScreen</div>
+      <div>
+        <div>
+          <Header />
+        </div>
+        <div className='flex flex-1 justify-center items-center h-[35rem]'>
+          <Paper elevation={3} className = "w-1/2 rounded-md">
+            <div className='flex flex-row h-96'>
+              <div className='h-full w-2/5 bg-gradient-to-r from-cyan-500 to-blue-500 flex flex-col items-start px-10 py-10'>
+                <span className='font-bold text-4xl text-gray-100'>SignUP</span>
+                <span className='text-sm '>For new Account</span>
+                <div className='mt-20'>
+                <Button variant="contained">LogIN</Button>
+                <br/>
+                <span className='text-xs text-white'>If Already have Account</span>
+                </div>
+              </div>
+              <div className='flex flex-col justify-center items-center w-3/5 h-full'>
+                
+                <div className='flex flex-row'>
+                  <AccountCircle sx={{ color: 'action.active', mr: 1, my: 2 }} />
+                  <TextField label="First Name" variant="standard" sx={{width:"18rem"}} />
+                </div>
+                <div className='flex flex-row'>
+                  <AccountCircle sx={{ color: 'action.active', mr: 1, my: 2 }} />
+                  <TextField label="First Name" variant="standard" sx={{width:"18rem"}} />
+                </div>
+                <div className='flex flex-row'>
+                  <Email sx={{ color: 'action.active', mr: 1, my: 2 }} />
+                  <TextField label="Email" variant="standard" sx={{width:"18rem"}} />
+                </div>
+                <div className='flex flex-row'>
+                  <Lock sx={{ color: 'action.active', mr: 1, my: 2 }} />
+                  <TextField label="Password" variant="standard" sx={{width:"18rem"}} />
+                </div>  
+              </div>
+            </div>
+          </Paper>
+        </div>
+      </div>
     )
   }
 }
