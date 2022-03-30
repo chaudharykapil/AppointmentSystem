14fa2b963cbb04eb4b5a6e306490911b3f90a063
chaudharykapil <chaudharykapil8279@gmail.com>
2022-03-31 00:07:53 +0530

    fix header and add link

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 export default function Header() {
   return (
     <div className='flex flex-row w-full px-5 py-3 font-bold bg-slate-800 text-white'>
-        <div className='flex-initial w-3/6 hover:underline'>Logo</div>
-        <div className='flex-1 hover:underline'>Home</div>
-        <div className='flex-1 hover:underline'>About us</div>
+        <div className='flex-initial w-3/6'>Logo</div>
+        <div className='flex-1 hover:underline'><Link to={"/"}>Home</Link></div>
+        <div className='flex-1 hover:underline'><Link to={"/faqs"}> About us</Link></div>
         <div className='flex-1 hover:underline'>View list</div>
         <div className='flex-1 hover:underline'>Sign up</div>
     </div>
