3e6591cdf2cac107b2f218819d80e1e0ff27818d
chaudharykapil <chaudharykapil8279@gmail.com>
2022-04-04 21:04:41 +0530

    fix links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,9 @@ export default function Header() {
     <div className='flex flex-row w-full px-5 py-3 font-bold bg-slate-800 text-white'>
         <div className='flex-initial w-3/6'>Logo</div>
         <div className='flex-1 hover:underline'><Link to={"/"}>Home</Link></div>
-        <div className='flex-1 hover:underline'><Link to={"/faqs"}> About us</Link></div>
-        <div className='flex-1 hover:underline'><Link to={"/addfaq"}>View list</Link></div>
-        <div className='flex-1 hover:underline'>Sign up</div>
+        <div className='flex-1 hover:underline'><Link to={"#"}> About us</Link></div>
+        <div className='flex-1 hover:underline'><Link to = {"#"}>View list</Link></div>
+        <div className='flex-1 hover:underline'><Link to = {"/signup"}>Sign up</Link></div>
     </div>
   )
 }
