d705fa42c550abf4e433c70e0bc2274b30dad00b
chaudharykapil <chaudharykapil8279@gmail.com>
2022-04-04 21:03:49 +0530

    add new routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddFaqScreen from "./screens/AddFaqScreen";
+import DashBoard from "./screens/DashBoard";
 import FaqScreen from "./screens/FaqScreen";
 import Homepage from "./screens/Homepage";
+import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
 function App() {
   return (
@@ -10,8 +12,10 @@ function App() {
       <Routes>
         <Route index element={<Homepage />} />
         <Route path = "/signup" element = {<SignUpScreen />} />
+        <Route path="/login" element = {<LoginScreen />} />
         <Route path="/faqs" element = {<FaqScreen />}/>
         <Route path="/addfaq" element = {<AddFaqScreen />} />
+        <Route path = "/dashboard" element = {<DashBoard />} />
       </Routes>
     </BrowserRouter>
   );
