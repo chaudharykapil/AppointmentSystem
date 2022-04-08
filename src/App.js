faaee3921017233ca9c559050b47e40e07b09517
chaudharykapil <chaudharykapil8279@gmail.com>
2022-04-08 21:05:53 +0530

    add nre routes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddDepartment from "./screens/addDepartment";
 import AddFaqScreen from "./screens/AddFaqScreen";
+import UserLoginScreen from "./screens/customer/LoginScreen";
+import UserSignUpScreen from "./screens/customer/SignUpScreen";
+import ViewList from "./screens/customer/ViewList";
 import DashBoard from "./screens/DashBoard";
 import FaqScreen from "./screens/FaqScreen";
 import Homepage from "./screens/Homepage";
 import LoginScreen from "./screens/LoginScreen";
 import SignUpScreen from "./screens/SignUpScreen";
+import WorkingDays from "./screens/WorkingDays";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Homepage />} />
         <Route path = "/signup" element = {<SignUpScreen />} />
-        <Route path="/login" element = {<LoginScreen />} />
-        <Route path="/faqs" element = {<FaqScreen />}/>
-        <Route path="/addfaq" element = {<AddFaqScreen />} />
+        <Route path = "/user/signup" element = {<UserSignUpScreen />} />
+        <Route path = "/addworking" element = {<WorkingDays />} />
+        <Route path = "/add-department" element = {<AddDepartment />} />
+        <Route path= "/login" element = {<LoginScreen />} />
+        <Route path= "/user/login" element = {<UserLoginScreen />} />
+        <Route path= "/faqs" element = {<FaqScreen />}/>
+        <Route path= "/addfaq" element = {<AddFaqScreen />} />
         <Route path = "/dashboard" element = {<DashBoard />} />
+        <Route path="/viewlist" element = {<ViewList />} />
       </Routes>
     </BrowserRouter>
   );
